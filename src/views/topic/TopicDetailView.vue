57b439a6bc4b90fb7d907142f3c3571345dfee04
Corleone <sakurayae2331@outlook.com>
2025-03-17 11:46:24 +0800

    修复了论坛部分bug

--- src/views/topic/TopicDetailView.vue
+++ src/views/topic/TopicDetailView.vue
@@ -8,28 +8,9 @@
 
 import Header from '@/components/layout/Header.vue';
 import Footer from '@/components/layout/Footer.vue';
-import { ref, onMounted } from 'vue';
-import { getTopicDetail } from '@/api/topic'; // 导入 API
-import { useRoute } from 'vue-router';
 import TopicDetail from '@/components/topic/TopicDetail.vue';
 
-const route = useRoute();
-const topic = ref(null);
 
-onMounted(() => {
-    fetchTopicDetail();
-});
-
-const fetchTopicDetail = () => {
-    getTopicDetail(route.params.id)
-        .then(response => {
-            topic.value = response.data.data; // 根据你的后端返回数据结构调整
-            console.log(topic.value);
-        })
-        .catch(error => {
-            console.error("Error fetching topic detail:", error);
-        });
-};
 </script>
 
 <style scoped>
